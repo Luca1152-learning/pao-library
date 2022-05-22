96c67b47d96e8a305f9716e3f3a2e4336c70e462
Luca1152 <t.luca1152@gmail.com>
2022-05-22 14:14:09 +0300

    Add PostgreSQL JDBC driver
    Now I can connect to the database.

--- app/src/main/java/pao/library/utils/Secrets.java
+++ app/src/main/java/pao/library/utils/Secrets.java
@@ -1,7 +1,7 @@
 package pao.library.utils;
 
 public enum Secrets {
-    DATABASE_URL("jdbc:postgresql://ec2-63-35-156-160.eu-west-1.compute.amazonaws.com:5432/derqc5g9ar8b4c?user=lsqucghuclddue&password=d83e9fe141a25b604107d8ad239c7dd615de3699a4266e4dea5fcdf0307115c9");
+    DATABASE_URL("jdbc:postgresql://ec2-63-35-156-160.eu-west-1.compute.amazonaws.com:5432/derqc5g9ar8b4c?sslmode=require&user=lsqucghuclddue&password=d83e9fe141a25b604107d8ad239c7dd615de3699a4266e4dea5fcdf0307115c9");
 
     private final String str;
 
