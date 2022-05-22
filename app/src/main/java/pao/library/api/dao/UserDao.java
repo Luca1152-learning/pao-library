ac1d868f4a14c9db71d484523f3dccb5db4e16d1
Luca1152 <t.luca1152@gmail.com>
2022-05-22 18:41:37 +0300

    Add full User DAO code
    It now all works.

--- app/src/main/java/pao/library/api/dao/UserDao.java
+++ app/src/main/java/pao/library/api/dao/UserDao.java
@@ -1,10 +1,13 @@
 package pao.library.api.dao;
 
-import pao.library.api.entities.User;
+import pao.library.api.model.Role;
+import pao.library.api.model.User;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class UserDao extends Dao<User, Integer> {
@@ -22,8 +25,9 @@ public class UserDao extends Dao<User, Integer> {
                 String hashedPassword = resultSet.getString("password");
                 String firstName = resultSet.getString("first_name");
                 String lastName = resultSet.getString("last_name");
+                String role = resultSet.getString("role");
 
-                user = new User(id, username, hashedPassword, firstName, lastName);
+                user = new User(id, username, hashedPassword, firstName, lastName, Role.fromString(role));
             }
             resultSet.close();
         } catch (SQLException e) {
@@ -35,21 +39,94 @@ public class UserDao extends Dao<User, Integer> {
 
     @Override
     public Collection<User> getAll() {
-        return null;
+        Collection<User> users = new ArrayList<>();
+
+        String sql = "SELECT * FROM users";
+        try (Statement statement = connection.createStatement(); ResultSet resultSet = statement.executeQuery(sql)) {
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String username = resultSet.getString("username");
+                String hashedPassword = resultSet.getString("password");
+                String firstName = resultSet.getString("first_name");
+                String lastName = resultSet.getString("last_name");
+                String role = resultSet.getString("role");
+
+                User user = new User(id, username, hashedPassword, firstName, lastName, Role.fromString(role));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return users;
     }
 
     @Override
-    public User save(User user) {
-        return null;
+    public Integer save(User user) {
+        if (user == null) {
+            throw new RuntimeException("The user to save shouldn't be null");
+        }
+
+        Integer generatedId = null;
+
+        String sql = "INSERT INTO users(username, password, first_name, last_name, role) VALUES (?, ?, ?, ?, ?)";
+        try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, user.getUsername());
+            statement.setString(2, user.getHashedPassword());
+            statement.setString(3, user.getFirstName());
+            statement.setString(4, user.getLastName());
+            statement.setString(5, user.getRole().toString());
+
+            int numberOfInstertedRows = statement.executeUpdate();
+
+            // Get the id
+            if (numberOfInstertedRows > 0) {
+                ResultSet resultSet = statement.getGeneratedKeys();
+                if (resultSet.next()) {
+                    generatedId = resultSet.getInt(1);
+                }
+                resultSet.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return generatedId;
     }
 
     @Override
     public void update(User user) {
+        if (user == null) {
+            throw new RuntimeException("The user to update shouldn't be null");
+        }
 
+        String sql = "UPDATE users " + "SET username=?, password=?, first_name=?, last_name=?, role=? " + "WHERE user_id=?";
+        try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, user.getUsername());
+            statement.setString(2, user.getHashedPassword());
+            statement.setString(3, user.getFirstName());
+            statement.setString(4, user.getLastName());
+            statement.setString(5, user.getRole().toString());
+            statement.setInt(6, user.getId());
+
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void delete(User user) {
+        if (user == null) {
+            throw new RuntimeException("The user to delete shouldn't be null");
+        }
 
+        String sql = "DELETE FROM users WHERE user_id = ?";
+        try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            statement.setInt(1, user.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
