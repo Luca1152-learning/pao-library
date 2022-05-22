066273ce88279a5fb456d98c473667651e84f657
Luca1152 <t.luca1152@gmail.com>
2022-05-22 19:44:20 +0300

    Add Book and BookDao
    Not tested yet, though.

---
+++ app/src/main/java/pao/library/api/dao/BookDao.java
@@ -0,0 +1,138 @@
+package pao.library.api.dao;
+
+import pao.library.api.model.Book;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class BookDao extends Dao<Book, Integer> {
+    @Override
+    public Book get(Integer id) {
+        Book book = null;
+
+        String sql = "SELECT * FROM books WHERE book_id=?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, id);
+
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                String title = resultSet.getString("title");
+                String description = resultSet.getString("password");
+                int publisherId = resultSet.getInt("publisher_id");
+                int firstPublicationYear = resultSet.getInt("first_publication_year");
+                int pagesCount = resultSet.getInt("pages_count");
+                int availableCopies = resultSet.getInt("available_copies");
+
+                book = new Book(id, title, description, publisherId, firstPublicationYear, pagesCount, availableCopies);
+            }
+            resultSet.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return book;
+    }
+
+    @Override
+    public Collection<Book> getAll() {
+        Collection<Book> books = new ArrayList<>();
+
+        String sql = "SELECT * FROM books";
+        try (Statement statement = connection.createStatement(); ResultSet resultSet = statement.executeQuery(sql)) {
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String title = resultSet.getString("title");
+                String description = resultSet.getString("password");
+                int publisherId = resultSet.getInt("publisher_id");
+                int firstPublicationYear = resultSet.getInt("first_publication_year");
+                int pagesCount = resultSet.getInt("pages_count");
+                int availableCopies = resultSet.getInt("available_copies");
+
+                Book book = new Book(id, title, description, publisherId, firstPublicationYear, pagesCount,
+                        availableCopies);
+                books.add(book);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return books;
+    }
+
+    @Override
+    public Integer save(Book book) {
+        if (book == null) {
+            throw new RuntimeException("The book to save shouldn't be null");
+        }
+
+        Integer generatedId = null;
+
+        String sql = "INSERT INTO books(title, description, publisher_id, first_publication_year, pages_count, " +
+                "available_copies) VALUES (?, ?, ?, ?, ?, ?)";
+        try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, book.getTitle());
+            statement.setString(2, book.getDescription());
+            statement.setInt(3, book.getPublisherId());
+            statement.setInt(4, book.getFirstPublicationYear());
+            statement.setInt(5, book.getPagesCount());
+            statement.setInt(6, book.getAvailableCopies());
+
+            int numberOfInstertedRows = statement.executeUpdate();
+
+            // Get the id
+            if (numberOfInstertedRows > 0) {
+                ResultSet resultSet = statement.getGeneratedKeys();
+                if (resultSet.next()) {
+                    generatedId = resultSet.getInt(1);
+                }
+                resultSet.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return generatedId;
+    }
+
+    @Override
+    public void update(Book book) {
+        if (book == null) {
+            throw new RuntimeException("The book to update shouldn't be null");
+        }
+
+        String sql = "UPDATE books SET title=?, description=?, publisher_id=?, first_publication_year=?, " +
+                "pages_count=?, available_copies = ? WHERE book_id=?";
+        try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, book.getTitle());
+            statement.setString(2, book.getDescription());
+            statement.setInt(3, book.getPublisherId());
+            statement.setInt(4, book.getFirstPublicationYear());
+            statement.setInt(5, book.getPagesCount());
+            statement.setInt(6, book.getAvailableCopies());
+            statement.setInt(7, book.getId());
+
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void delete(Book book) {
+        if (book == null) {
+            throw new RuntimeException("The book to delete shouldn't be null");
+        }
+
+        String sql = "DELETE FROM books WHERE book_id = ?";
+        try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            statement.setInt(1, book.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/pao/library/api/model/Book.java
@@ -0,0 +1,100 @@
+package pao.library.api.model;
+
+import java.util.Objects;
+
+public class Book extends BaseEntity<Integer> {
+    private String title;
+    private String description;
+    private int publisherId;
+    private int firstPublicationYear;
+    private int pagesCount;
+    private int availableCopies;
+
+    public Book(int id, String title, String description, int publisherId, int firstPublicationYear, int pagesCount,
+                int availableCopies) {
+        super(id);
+        this.title = title;
+        this.description = description;
+        this.publisherId = publisherId;
+        this.firstPublicationYear = firstPublicationYear;
+        this.pagesCount = pagesCount;
+        this.availableCopies = availableCopies;
+    }
+
+    public Book(String title, String description, int publisherId, int firstPublicationYear, int pagesCount,
+                int availableCopies) {
+        super(null);
+        this.title = title;
+        this.description = description;
+        this.publisherId = publisherId;
+        this.firstPublicationYear = firstPublicationYear;
+        this.pagesCount = pagesCount;
+        this.availableCopies = availableCopies;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPublisherId() {
+        return publisherId;
+    }
+
+    public void setPublisherId(int publisherId) {
+        this.publisherId = publisherId;
+    }
+
+    public int getFirstPublicationYear() {
+        return firstPublicationYear;
+    }
+
+    public void setFirstPublicationYear(int firstPublicationYear) {
+        this.firstPublicationYear = firstPublicationYear;
+    }
+
+    public int getPagesCount() {
+        return pagesCount;
+    }
+
+    public void setPagesCount(int pagesCount) {
+        this.pagesCount = pagesCount;
+    }
+
+    public int getAvailableCopies() {
+        return availableCopies;
+    }
+
+    public void setAvailableCopies(int availableCopies) {
+        this.availableCopies = availableCopies;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Book[id = " + id + ", title=" + title + "]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book author = (Book) o;
+        return title.equals(author.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title);
+    }
+}
