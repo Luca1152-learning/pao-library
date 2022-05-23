055bc5b0a481c7351b0fb613a0c50fad0f20bfe1
Luca1152 <t.luca1152@gmail.com>
2022-05-23 15:57:19 +0300

    Add authentication I/O
    Now the user can authenticate.

--- app/src/main/java/pao/library/io/AppIo.java
+++ app/src/main/java/pao/library/io/AppIo.java
@@ -4,7 +4,10 @@ import pao.library.api.model.User;
 
 public class AppIo {
     public void prompt() {
+        // Authentication
         System.out.println("Welcome!\n");
         User user = (new AuthIo()).prompt();
+
+        System.out.println("\nWelcome, " + user.getFirstName() + " " + user.getLastName() + "!");
     }
 }
--- app/src/main/java/pao/library/io/AuthIo.java
+++ app/src/main/java/pao/library/io/AuthIo.java
@@ -3,21 +3,76 @@ package pao.library.io;
 import pao.library.api.model.User;
 import pao.library.api.service.UserService;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class AuthIo {
+    private static final NumberedOptions AUTH_OPTIONS = new NumberedOptions(new ArrayList<>(Arrays.asList(
+            "Sign up", // 1
+            "Sign in" // 2
+    )));
     private static final UserService USER_SERVICE = new UserService();
 
     public User prompt() {
         System.out.println("Please sign in / sign up.");
 
-//        try {
-//            userService.signUp("username", "parola", "Prenume", "Nume");
-//        } catch (SQLException exception) {
-//            System.out.println("Sign up error. Try again!");
-//        }
+        int option = AUTH_OPTIONS.prompt();
+        if (option == 1) {
+            // Sign up
+            while (true) {
+                System.out.println();
+                String username = promptUsername();
+                String password = promptPassword();
+                String firstName = promptFirstName();
+                String lastName = promptLastName();
+                try {
+                    return USER_SERVICE.signUp(username, password, firstName, lastName);
+                } catch (SQLException exception) {
+                    // Probably there's a duplicate username
+                    System.out.println("\nSign up error. Try again.");
+                }
+            }
+        } else if (option == 2) {
+            // Sign in
+            while (true) {
+                System.out.println();
+                String username = promptUsername();
+                String password = promptPassword();
+                User user = USER_SERVICE.signIn(username, password);
+                if (user != null) {
+                    return user;
+                } else {
+                    System.out.println("\nBad credentials. Try again.");
+                }
+            }
+        } else {
+            throw new RuntimeException("Option not handled.");
+        }
+    }
 
-        User user = USER_SERVICE.signIn("usernamee", "parola");
-        System.out.println(user.getFirstName());
+    private String promptUsername() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Username: ");
+        return scanner.next();
+    }
 
-        return null;
+    private String promptPassword() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Password: ");
+        return scanner.next();
+    }
+
+    private String promptFirstName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("First name: ");
+        return scanner.next();
+    }
+
+    private String promptLastName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Last name: ");
+        return scanner.next();
     }
 }
